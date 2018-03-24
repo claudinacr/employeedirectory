4a1ee37bbde75602f46f64664a90750587e1b322
Claudina Cortez de Leon <claudinacr@gmail.com>
2018-03-24 20:33:06 -0300

    Implementacion de componentes

--- src/Components/App/EmployeePage/Containeremployee/Dataemployee.js
+++ src/Components/App/EmployeePage/Containeremployee/Dataemployee.js
@@ -1,5 +1,17 @@
 import React from 'react'
-const Dataemployee = () =>(
-  <div className='Dataemployee'>Dataemployee</div>
+const Dataemployee = () => (
+  <div className='Dataemployee'>
+    <div className='Contdata'>
+      <div className='dataphoto'></div>
+      <div>
+        <div className='datanamee'></div>
+        <div className='datajobb'></div>
+      </div>
+    </div>
+    <div className='calloffice'></div>
+    <div className='callmovile'></div>
+    <div className='sms'></div>
+    <div className='email'></div>
+  </div>
 )
 export default Dataemployee;
--- src/Components/App/HomePage/Containerhomepage/EmployeeList.js
+++ src/Components/App/HomePage/Containerhomepage/EmployeeList.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
 import data from './../../../../data/data';
+import Dataemployee from './../../EmployeePage/Containeremployee/Dataemployee';
+
 const Employeelist = () => {
+
   return (
     data.map((job) =>
-      <div className='Employeelist' >
+      <div className='Employeelist'  key ={job.id} onClick={this.Search =()=>{
+        document.querySelector('.dataphoto').appendChild(document.createElement('img')).setAttribute('src', job.photo);
+        document.querySelector('.datanamee').appendChild(document.createElement('span')).appendChild(document.createTextNode(job.name))
+        document.querySelector('.datajobb').appendChild(document.createElement('span')).appendChild(document.createTextNode(job.position))
+
+        document.querySelector('.calloffice').appendChild(document.createElement('span')).appendChild(document.createTextNode(job.Office))
+        document.querySelector('.callmovile').appendChild(document.createElement('span')).appendChild(document.createTextNode(job.Mobile))
+        document.querySelector('.sms').appendChild(document.createElement('span')).appendChild(document.createTextNode(job.sms))
+        document.querySelector('.email').appendChild(document.createElement('span')).appendChild(document.createTextNode(job.email))
+
+
+        }}>
         <div className='employeephoto'>
           <img className='photo' src={job.photo} />
         </div>
@@ -11,7 +25,7 @@ const Employeelist = () => {
           <span className='jobname'>{job.name}</span>
           <span className='jobposition'>{job.position}</span>
         </div>
-        <div></div>
+
       </div>
     )
 
